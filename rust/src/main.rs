7e365b6d086c7dc24a723e92648522211bc7aa43
Frank <frank@void0.ch>
2024-04-12 21:32:26 +0200

    make rust server work

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,4 +1,3 @@
-use std::env::var;
 use std::sync::Mutex;
 use actix::{Actor, StreamHandler};
 use actix_web::{web, get, post, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
@@ -34,22 +33,58 @@ struct Vote {
     client: String,
 }
 
+#[derive(Serialize, Deserialize, Clone)]
+struct VoteRequest {
+    id: u32,
+    answer: Answer,
+    client_id: String
+}
+
+#[get("/poll/{poll_id}")]
+async fn get_poll(
+    data: web::Data<crate::AppState>,
+    path: web::Path<u32>,
+) -> Result<HttpResponse, Error> {
+    let poll_id = path.into_inner();
+    let all_polls = data.all_polls.lock().unwrap();
+    let poll = all_polls.iter().find(| x| x.id == poll_id);
+    Ok(HttpResponse::Ok().json(poll))
+}
 
 #[post("/poll")]
-async fn createPoll(poll: web::Json<Poll>, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+async fn create_poll(poll: web::Json<Poll>, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
     let mut all_polls = data.all_polls.lock().unwrap();
     all_polls.push(poll.0.clone());
     Ok(HttpResponse::Ok().json(poll))
 }
 
+#[post("/vote")]
+async fn vote(vote_request: web::Json<VoteRequest>, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+    let mut all_polls = data.all_polls.lock().unwrap();
+
+    // Find the poll
+    if let Some(poll) = all_polls.iter_mut().find(|p| p.id == vote_request.id) {
+        // Find the answer
+        if let Some(answer) = poll.answers.as_mut().and_then(|answers| answers.iter_mut().find(|a| a.id == vote_request.answer.id)) {
+            // Push a new vote into the votes vector
+            answer.votes.push(Vote { client: "example_client".to_string() });
+            println!("Vote added successfully!");
+        } else {
+            println!("Answer with the id {} not found", vote_request.answer.id)
+        }
+    } else {
+        println!("Poll with the id {} not found", vote_request.id)
+    }
+
+
+    Ok(HttpResponse::Ok().body("done!"))
+}
+
 #[get("/poll")]
-async fn getPolls(data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+async fn get_polls(data: web::Data<AppState>) -> Result<HttpResponse, Error> {
     Ok(HttpResponse::Ok().json(&data.all_polls))
 }
 
-async fn manual_hello() -> impl Responder {
-    HttpResponse::Ok().body("Hey there!")
-}
 
 /// Define HTTP actor
 struct MyWs;
@@ -91,8 +126,8 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(move || App::new()
         .app_data(state.clone())
         .service(hello)
-        .service(createPoll)
-        .service(getPolls)
+        .service(create_poll)
+        .service(get_polls)
         .route("/ws/", web::get().to(index)))
         .bind(("127.0.0.1", 8080))?
         .run()
