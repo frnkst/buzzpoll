98c15f0755d8227ef87aab4ccf0918c1014aa1af
Frank <frank@void0.ch>
2024-04-15 21:58:17 +0200

    well this is nice

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,66 +1,11 @@
 mod model;
+mod services;
 
 use std::sync::Mutex;
 use actix::{Actor, StreamHandler};
-use actix_web::{web, get, post, App, Error, HttpRequest, HttpResponse, HttpServer, Responder};
+use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
 use actix_web_actors::ws;
-use model::{Poll, VoteRequest };
-
-#[get("/")]
-async fn hello() -> impl Responder {
-    HttpResponse::Ok().body("Hello world!")
-}
-
-#[post("/echo")]
-async fn echo(req_body: String) -> impl Responder {
-    HttpResponse::Ok().body(req_body)
-}
-
-
-#[get("/poll/{poll_id}")]
-async fn get_poll(
-    data: web::Data<crate::AppState>,
-    path: web::Path<u32>,
-) -> Result<HttpResponse, Error> {
-    let poll_id = path.into_inner();
-    let all_polls = data.all_polls.lock().unwrap();
-    let poll = all_polls.iter().find(| x| x.id == poll_id);
-    Ok(HttpResponse::Ok().json(poll))
-}
-
-#[post("/poll")]
-async fn create_poll(poll: web::Json<Poll>, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
-    let mut all_polls = data.all_polls.lock().unwrap();
-    all_polls.push(poll.0.clone());
-    Ok(HttpResponse::Ok().json(poll))
-}
-
-#[post("/vote")]
-async fn vote(vote_request: web::Json<VoteRequest>, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
-    let mut all_polls = data.all_polls.lock().unwrap();
-
-    // Find the poll
-    if let Some(poll) = all_polls.iter_mut().find(|p| p.id == vote_request.id) {
-        // Find the answer
-        if let Some(answer) = poll.answers.as_mut().and_then(|answers| answers.iter_mut().find(|a| a.id == vote_request.answer.id)) {
-            // Push a new vote into the votes vector
-            answer.votes.push(model::Vote { client: "example_client".to_string() });
-            println!("Vote added successfully!");
-        } else {
-            println!("Answer with the id {} not found", vote_request.answer.id)
-        }
-    } else {
-        println!("Poll with the id {} not found", vote_request.id)
-    }
-
-
-    Ok(HttpResponse::Ok().body("done!"))
-}
-
-#[get("/poll")]
-async fn get_polls(data: web::Data<AppState>) -> Result<HttpResponse, Error> {
-    Ok(HttpResponse::Ok().json(&data.all_polls))
-}
+use crate::services::{create_poll, get_polls, hello};
 
 
 /// Define HTTP actor
---
+++ rust/src/services.rs
@@ -0,0 +1,59 @@
+use actix_web::{Error, get, HttpResponse, post, Responder, web};
+use crate::{AppState, model};
+use crate::model::{VoteRequest, Poll};
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello world!")
+}
+
+#[post("/echo")]
+async fn echo(req_body: String) -> impl Responder {
+    HttpResponse::Ok().body(req_body)
+}
+
+
+#[get("/poll/{poll_id}")]
+async fn get_poll(
+    data: web::Data<crate::AppState>,
+    path: web::Path<u32>,
+) -> Result<HttpResponse, Error> {
+    let poll_id = path.into_inner();
+    let all_polls = data.all_polls.lock().unwrap();
+    let poll = all_polls.iter().find(| x| x.id == poll_id);
+    Ok(HttpResponse::Ok().json(poll))
+}
+
+#[post("/poll")]
+async fn create_poll(poll: web::Json<Poll>, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+    let mut all_polls = data.all_polls.lock().unwrap();
+    all_polls.push(poll.0.clone());
+    Ok(HttpResponse::Ok().json(poll))
+}
+
+#[post("/vote")]
+async fn vote(vote_request: web::Json<VoteRequest>, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+    let mut all_polls = data.all_polls.lock().unwrap();
+
+    // Find the poll
+    if let Some(poll) = all_polls.iter_mut().find(|p| p.id == vote_request.id) {
+        // Find the answer
+        if let Some(answer) = poll.answers.as_mut().and_then(|answers| answers.iter_mut().find(|a| a.id == vote_request.answer.id)) {
+            // Push a new vote into the votes vector
+            answer.votes.push(model::Vote { client: "example_client".to_string() });
+            println!("Vote added successfully!");
+        } else {
+            println!("Answer with the id {} not found", vote_request.answer.id)
+        }
+    } else {
+        println!("Poll with the id {} not found", vote_request.id)
+    }
+
+
+    Ok(HttpResponse::Ok().body("done!"))
+}
+
+#[get("/poll")]
+async fn get_polls(data: web::Data<AppState>) -> Result<HttpResponse, Error> {
+    Ok(HttpResponse::Ok().json(&data.all_polls))
+}
