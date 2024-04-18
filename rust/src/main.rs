2d7f1516a3aa10b52812ec11674c107481067ec1
Frank <frank@void0.ch>
2024-04-18 20:55:18 +0200

    maybe almost works?

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -1,87 +1,99 @@
-mod model;
-mod services;
-
-use crate::model::{AppState};
-use crate::services::{create_poll, get_poll, get_polls, vote};
-use actix::{Actor, ActorContext, Addr, Context, StreamHandler};
-use actix_web::{web, App, Error, HttpRequest, HttpResponse, HttpServer};
+use crate::web::Data;
+use std::sync::{Arc, RwLock};
+use actix::{Actor, StreamHandler};
+use actix_web::{App, HttpRequest, HttpResponse, HttpServer, Responder, web};
+use actix_web::web::route;
 use actix_web_actors::ws;
-use std::sync::{Arc, Mutex};
-use actix_web::web::Data;
-use actix_web_actors::ws::WebsocketContext;
 
+struct Game {
+    websockets: Vec<Arc<RwLock<MyWs>>>,
+}
 
-/// Define HTTP actor
-struct MyWs {}
+impl Game {
+    pub fn new() -> Self {
+        Game { websockets: vec![] }
+    }
 
+    pub fn add_websocket(&mut self, my_ws: Arc<RwLock<MyWs>>) {
+        self.websockets.push(my_ws);
+    }
 
+    pub fn some_method(&mut self) {
+        // Do something to influence internal state.
+        self.push_state();
+    }
 
-impl Actor for MyWs {
-    type Context = WebsocketContext<Self>;
-
-    fn started(&mut self, ctx: &mut Self::Context) {
-
-
-
-        //println!("pushed one client");
-        // &self.app_data.clients.lock().unwrap().push(ctx);
-        // println!("number of clients {:?}", &self.app_data.clients.lock().unwrap().len());
+    pub fn push_state(&self) {
+        for w in &self.websockets {
+            // Send updates to clients using the WebsocketContext.
+            //w.write().unwrap().ctx.text("hello");
+        }
     }
 }
 
-/// Handler for ws::Message message
+struct MyWs {
+    game: Arc<RwLock<Game>>,
+}
+
+impl Actor for MyWs {
+    type Context = ws::WebsocketContext<Self>;
+}
+
 impl StreamHandler<Result<ws::Message, ws::ProtocolError>> for MyWs {
     fn handle(&mut self, msg: Result<ws::Message, ws::ProtocolError>, ctx: &mut Self::Context) {
-        //println!("schlafen {:?}", msg);
-
-        // for ctx in &self.state.clients {
-        //     unsafe { ctx.as_mut().expect("REASON").text("well well well"); }
-        // }
-
-
         match msg {
-            Ok(ws::Message::Ping(msg)) => ctx.pong(&msg),
-            Ok(ws::Message::Text(_)) => ctx.text("frank"),
-            Ok(ws::Message::Binary(bin)) => ctx.binary(bin),
+            Ok(ws::Message::Text(text)) => {
+                self.game.write().unwrap().some_method();
+                ctx.text(text);
+            }
             _ => (),
         }
     }
 }
 
-async fn index(req: HttpRequest, stream: web::Payload, data: web::Data<AppState>) -> Result<HttpResponse, Error> {
-
-    //let state = Data::new(WsAppState {
-    //    clients: Mutex::new(Vec::new()),
-    //});
-    let actor = MyWs {};
-    &data.clients.lock().unwrap().push(*actor);
-
-    // let resp = ws::start(MyWs { app_data: state.clone() }, &req, stream);
-    let resp = ws::start(actor, &req, stream);
-    //println!("frank {:?}", resp);
-    resp
+struct GameWrapper {
+    game: Arc<RwLock<Game>>,
 }
+
+impl GameWrapper {
+    pub fn new(game: Arc<RwLock<Game>>) -> Self {
+        GameWrapper { game }
+    }
+}
+
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     std::env::set_var("RUST_LOG", "debug");
     env_logger::init();
-
-    let state = web::Data::new(AppState {
-        all_polls: Mutex::new(Vec::new()),
-        clients: Mutex::new(Vec::new()),
-    });
+    let game = Arc::new(RwLock::new(Game::new()));
+    let game_wrapper = RwLock::new(GameWrapper::new(game.clone()));
+    let game_wrapper_data = web::Data::new(game_wrapper);
 
     HttpServer::new(move || {
         App::new()
-            .app_data(state.clone())
-            .service(get_polls)
-            .service(get_poll)
-            .service(create_poll)
-            .service(vote)
-            .route("/ws/", web::get().to(index))
+            .app_data(game_wrapper_data.clone())
+            .route("/play_game", web::get().to(play_game))
     })
-    .bind(("127.0.0.1", 8080))?
-    .run()
-    .await
+        .bind("127.0.0.1:8080")?
+        .run()
+        .await
+}
+
+pub async fn play_game(
+    req: HttpRequest,
+    stream: web::Payload,
+    game_wrapper: web::Data<GameWrapper>,
+) -> impl Responder {
+    let my_ws = MyWs {
+        game: game_wrapper.game.clone(),
+    };
+    //let my_ws = Arc::new(RwLock::new(my_ws));
+    //let my_ws = MyWs { game: "bla"};
+
+    let mut game = game_wrapper.game.write().unwrap();
+    //game.add_websocket(my_ws.clone());
+
+    let resp = ws::start(my_ws, &req, stream);
+    resp.unwrap_or_else(|e| HttpResponse::from_error(e))
 }
