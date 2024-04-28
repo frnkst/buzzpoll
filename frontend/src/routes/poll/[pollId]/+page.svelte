ad56e521a5b177881f1f2cbfe46a30f54fbc711b
Frank <frank@void0.ch>
2024-04-28 18:28:08 +0200

    add echarts and a bar race

--- frontend/src/routes/poll/[pollId]/+page.svelte
+++ frontend/src/routes/poll/[pollId]/+page.svelte
@@ -2,11 +2,85 @@
 	import { page } from '$app/stores';
 	import { onMount } from "svelte";
 	import {writable} from "svelte/store";
+	import * as echarts from 'echarts';
 
 	console.log("frank", $page.params.pollId);
   let poll;
 
+  function chart() {
+		var myChart = echarts.init(document.getElementById('main'));
+
+		var data = [];
+		for (let i = 0; i < 5; ++i) {
+			data.push(Math.round(Math.random() * 200));
+		}
+
+		const option = {
+			xAxis: {
+				max: 'dataMax'
+			},
+			yAxis: {
+				type: 'category',
+				data: ['A', 'B', 'C', 'D', 'E'],
+				inverse: true,
+				animationDuration: 300,
+				animationDurationUpdate: 300,
+				max: 2 // only the largest 3 bars will be displayed
+			},
+			series: [
+				{
+					realtimeSort: true,
+					name: 'X',
+					type: 'bar',
+					data: data,
+					label: {
+						show: true,
+						position: 'right',
+						valueAnimation: true
+					}
+				}
+			],
+			legend: {
+				show: true
+			},
+			animationDuration: 0,
+			animationDurationUpdate: 3000,
+			animationEasing: 'linear',
+			animationEasingUpdate: 'linear'
+		};
+
+		function run() {
+			var data = option.series[0].data;
+			for (var i = 0; i < data.length; ++i) {
+				if (Math.random() > 0.9) {
+					data[i] += Math.round(Math.random() * 2000);
+				} else {
+					data[i] += Math.round(Math.random() * 200);
+				}
+			}
+			myChart.setOption(option);
+		}
+
+		myChart.setOption(option);
+
+		setTimeout(function() {
+			run();
+		}, 0);
+		setInterval(function() {
+			run();
+		}, 3000);
+
+  }
+
 	onMount(async function () {
+
+  chart();
+
+
+
+
+
+
 		console.log("trying");
 		const endpoint = "http://localhost:8080/poll/" + $page.params.pollId;
 
@@ -70,6 +144,9 @@
     <button on:click={() => submit(answer.id)}>{ answer.text } </button>
   {/each}
 {/if}
+
+
+<div id="main" style="width:600px; height:400px;"></div>
 
 
 
